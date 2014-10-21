1e1d073316dd22a3da1e166fe763aeb019dc690c
Benjamin Clarke <benjiclarke@me.com>
2014-10-21 09:23:08 +0100

    Added parameter functionality to compilation script

--- COMPILE_AND_RUN.sh
+++ COMPILE_AND_RUN.sh
@@ -2,10 +2,15 @@
 
 FILENAME=$1;
 COMPILEDFILE=`echo $FILENAME | awk -F. '{ print $1 }'`;
-
+ARGV1=$2;
 main() {
 	
-	gcc -Wall -pedantic -O3 -lm -std=c99 $FILENAME -o .compiled/$COMPILEDFILE && .compiled/./$COMPILEDFILE;
+	gcc -Wall -pedantic -O3 -lm -std=c99 $FILENAME -o .compiled/$COMPILEDFILE; 
+	if [ -z "$ARGV1" ]; then
+		.compiled/./$COMPILEDFILE;
+	else
+		.compiled/./$COMPILEDFILE $ARGV1;
+	fi
 }
 
 main
